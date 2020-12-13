e19912f258bcc33bf59c502c425197a9c41bfaff
hewangtong <hwt0521@126.com>
2020-12-13 17:55:29 +0800

    bansepage增加注释

--- page/basepage.py
+++ page/basepage.py
@@ -35,7 +35,7 @@ class BasePage:
         return self._driver.find_elements(by, locator)
 
     def wait_for_click(self, locator):
-        #显示等待
+        #显示等待,传递的参数是元组
         WebDriverWait(self._driver, 10).until(expected_conditions.element_to_be_clickable(locator))
 
     def wait_for_condition(self, condition):
